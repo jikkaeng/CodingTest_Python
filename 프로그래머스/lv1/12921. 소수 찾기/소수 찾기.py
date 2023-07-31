6bf6863906ab457ff54c8e42571adc5773708bd5
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-07-31 18:24:27 +0900

    [level 1] Title: 소수 찾기, Time: 2928.80 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    check=0
+    for i in range(2,n+1):
+        for j in range(2,int(i**(0.5))+1):
+            if i%j==0:
+                check=1
+                break
+        if check!=1:
+            answer+=1
+        check=0
+    return answer
