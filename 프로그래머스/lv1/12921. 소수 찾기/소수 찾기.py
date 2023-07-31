c01d574d67d5aec1d030ce5fc9a8d3c6a7dd0296
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-07-31 20:44:46 +0900

    [level 1] Title: 소수 찾기, Time: 91.43 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,5 +1,6 @@
 def solution(n):
     answer = 0
+    '''
     check=0
     for i in range(2,n+1):
         for j in range(2,int(i**(0.5))+1):
@@ -9,4 +10,12 @@ def solution(n):
         if check!=1:
             answer+=1
         check=0
+    '''
+    sieve = [True] * (n+1)
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i] == True:           # i가 소수인 경우
+            for j in range(i+i, n+1, i): # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+    answer=sieve.count(True)-2
     return answer
