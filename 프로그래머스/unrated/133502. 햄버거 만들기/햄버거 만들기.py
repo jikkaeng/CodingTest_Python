27d38039b03586ceccce37e9d710b18c79a330bc
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-08-02 17:52:14 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,10 @@
+def solution(ingredient):
+    answer = 0
+    li=[]
+    for i in ingredient:
+        li.append(i)
+        if li[-4:]==[1,2,3,1]:
+            answer+=1
+            for j in range(4):
+                li.pop()
+    return answer
