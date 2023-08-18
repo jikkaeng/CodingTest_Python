233a8cbe61d7efede4154294d677e1148fe0b12c
JIYEON YU <128116477+jikkaeng@users.noreply.github.com>
2023-08-19 01:53:22 +0900

    [unrated] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,14 @@
+def solution(arr1, arr2):
+    answer = 0
+    if len(arr1)>len(arr2):
+        answer=1
+    elif len(arr1)<len(arr2):
+        answer=-1
+    else:
+        if sum(arr1)==sum(arr2):
+            answer=0
+        elif sum(arr1)>sum(arr2):
+            answer=1
+        else:
+            answer=-1
+    return answer
